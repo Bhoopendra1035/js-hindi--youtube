293b5656207627ab86c64d96c5f2aff36b6f22c7
bhupendra tale <118257719+Bhoopendra1035@users.noreply.github.com>
2025-08-24 08:42:14 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,77 @@
+
+//string to number convesion............
+
+/*let score = 33;
+console.log(typeof score); //Number
+let stringNumber = String(score);
+console.log(stringNumber);// 33
+console.log(typeof stringNumber); // String*/
+
+// Number to String conversion..........
+
+/*let name = "bhupendra"
+let numberString = Number(name);
+console.log(numberString); //NaN
+console.log(typeof numberString);//NUmber*/
+
+//boolean to number.............
+
+/*let isLogIn = true;
+console.log(typeof isLogIn); // boolean
+ let numberBoolean = Number(isLogIn);
+ console.log(typeof numberBoolean);// number
+ console.log(numberBoolean);// true- 1, false - 0*/
+
+// Number to Boolean.................................
+
+/*let score = 22;
+let age ;
+let booleanNUmber = Boolean(age);
+let booleanNUmber1 = Boolean(score);
+
+console.log(typeof score); // number
+console.log(typeof booleanNUmber);//boolean
+console.log(booleanNUmber);// false
+console.log(booleanNUmber1);// true*/
+
+// string to boolean .....................
+
+/*let name = ""
+let name_01 = "bhupendra"
+
+console.log(typeof name); // string
+
+let booleanString = Boolean(name);
+let booleanString1 = Boolean(name_01);
+console.log(typeof booleanString);//boolean
+console.log(booleanString);//flase
+console.log(booleanString1);//true*/
+
+// boolran to string..............................
+
+let isLOgIn = false ;
+console.log(typeof isLOgIn);// boolean
+
+let stringBoolean = String(isLOgIn);
+console.log(stringBoolean);// false
+console.log(typeof stringBoolean);// string
+
+
+
+
+
+
+ 
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
