39b7ebfc05864af8f013486b26de15dc892842d4
bhupendra tale <118257719+Bhoopendra1035@users.noreply.github.com>
2025-08-24 12:16:06 +0000

    comperation

---
+++ 01_basics/04_comperation.js
@@ -0,0 +1,26 @@
+// console.log(2>=2);
+// console.log(2<2);
+// console.log(6>2);
+// console.log(1<2);
+// console.log(2==2);
+// console.log(2 != 9);
+
+// console.log("3"==2);// false
+// console.log("3">2);// true
+
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null<=0)//true
+
+//************(datatype also check in ===)****** */
+console.log("2"=== 2)//false
+console.log(null===0)//false
+
+
+
+
+
+
+
+
+
