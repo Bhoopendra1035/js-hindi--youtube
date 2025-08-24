6110054854a8294521f8655adf53ed76b596a4b4
bhupendra tale <118257719+Bhoopendra1035@users.noreply.github.com>
2025-08-24 05:52:12 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+let name = "bhupendra";
+let age = 22;
+let isLogIn = false;
+let state;
+let temprature = null;
+
+// name = string
+// age -number
+// isLogIn - boolean
+// state - undefind
+// null - object
+
+console.log(typeof name);// string datatype
+console.log(typeof age);// number datatype
+console.log(typeof isLogIn)//boolean datatype
+console.log(typeof state)//undefined datatype
+console.log(typeof temprature)// object datatype
+
+
