965f26a3d737680471d48b40dff27cda72724b17
bhupendra tale <118257719+Bhoopendra1035@users.noreply.github.com>
2025-08-24 05:14:27 +0000

    #

---
+++ 01_basics/01_veriable.js
@@ -0,0 +1,17 @@
+const accountId = 122133
+let accountEmail = "bhupe.com"
+var accountPassword = "12333"
+accountCity = "bhopal" 
+
+// accountId = 2 not allowed
+
+accountPassword = "12333"
+accountEmail ="cchotu.com"
+accountCity = "hydrabad"
+
+let accountState ;
+
+/*
+preper no to use var, because of issue in block scope and functional scope*/
+
+console.table([accountCity,accountEmail, accountPassword,accountState ])
