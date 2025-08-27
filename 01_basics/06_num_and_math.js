9fbd920b3791c810c14977cda4b57417d2a335fc
bhupendra tale <118257719+Bhoopendra1035@users.noreply.github.com>
2025-08-27 05:54:53 +0000

    Num and Math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,69 @@
+// Number*******************
+
+/*const  score = 400; // comman way for decleration Number
+console.log(score);
+
+const balance = new Number(100) // In this way you find datatype with value.
+console.log(balance);
+
+// Some property of Number .........
+
+const otherNum = 10000.3333;
+
+console.log(otherNum.toString());// number change in string
+console.log(otherNum.toString().length) // find length of a number
+
+console.log(otherNum.toFixed(2)); // 123.2313312- . ke bad 2 value tak hi btayega..
+console.log(otherNum.toLocaleString('en-IN')); // 10000 to change 10,000*/
+
+//************Math***********************************************************
+
+/*console.log(Math); // go to googlr inspact console and copy this code and show some pROPERTY of math.
+
+console.log(Math.abs(-122));//  minus to plus (abs-abslute)
+
+console.log(Math.round(342.22)); // ignore point value (342)
+
+console.log(Math.ceil(4.2)); //  actual value increase abs one value -5 
+console.log(Math.floor(4.2));// ignore point value -4
+
+console.log(Math.min(3,35,62,12,1,9)); // find minimum value
+console.log(Math.max(2322,3435,657,86,4223,556));// find maximun value
+
+console.log(Math.pow(5,2)); // for power
+
+console.log(Math.random()); // generate random value between 0.0... to  9.9.....
+
+console.log((Math.random()*10) +1); // formula for random value bw 1....to 10.....
+
+console.log((Math.floor(Math.random()*10) +1)); // formula for random value bw  exact 1 to 10 value.*/
+
+// let min = 10;
+// let max = 20;
+
+// console.log(Math.floor((Math.random()*10) + (min+1))); 10-20
+
+/*let min = 10;
+let max = 23;
+
+console.log(Math.floor(Math.random()*(max-min +1) + min )); // 10-23 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
