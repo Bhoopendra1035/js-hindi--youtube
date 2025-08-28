1f0c3ff7878cc2876cea6b3588b2367f996f213d
bhupendra tale <118257719+Bhoopendra1035@users.noreply.github.com>
2025-08-28 05:22:10 +0000

    INterview Question

---
+++ 01_basics/test_1.js
@@ -0,0 +1,33 @@
+//LOgic Operation
+
+//Q.1: Check if a number is positive and even.
+//  Q.2: Check if a number is between 100 and 200.
+//  Q.3: Find if a number is between 10 and 50 using logical operators.
+
+
+//🔹 Functions
+
+//Q: Write a function to calculate factorial of a number. 
+// Q.2 : Write a function to find the maximum of two numbers.
+//  Q.3: create a Arrow function and make it any program acoding to you
+
+
+//🔹 Conditional Statements
+
+//Q: Check if a number is divisible by 3 and 5.  
+// Q.2 : Check if a year is a leap year. 
+// Q.3 Q: Write code using switch to print day of the week.  Q.4 Write a program to check if a number is even or odd. Q.5 Q: Explain ternary operator with example.
+
+//🔹 Loops
+
+//Q: Print numbers from 1 to 10 using a for loop. 
+// Q.2 Write code to print the sum of numbers from 1 to 100.
+// Q.3 Use for...of to print elements of an array. Q.4 Q: Write nested loops to print a pattern:
+/*
+
+*
+**
+***
+***** 
+
+*/
