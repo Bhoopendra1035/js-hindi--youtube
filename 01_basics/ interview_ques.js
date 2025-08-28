a7a0afb1bedd304ba79d38e607b59410dd144c6e
bhupendra tale <118257719+Bhoopendra1035@users.noreply.github.com>
2025-08-28 05:22:53 +0000

    Interviws Question

---
+++ 01_basics/ interview_ques.js
@@ -0,0 +1,62 @@
+/*
+🔹 Logical Operators
+
+What are logical operators in JavaScript?
+
+Explain the difference between && (AND), || (OR), and ! (NOT).
+
+What will be the output of this code?
+
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
+
+
+Can you explain short-circuit evaluation in logical operators?
+
+Write a program using && and || to check if a number is between 10 and 20.*/
+
+
+/*
+🔹 Functions
+
+What is a function in JavaScript and why do we use it?
+
+Difference between function declaration and function expression?
+
+What are arrow functions and how are they different from normal functions?
+
+Explain the concept of parameters and arguments in a function.
+
+Write a function to calculate the factorial of a number.*/
+
+
+/*
+
+🔹 Conditional Statements
+
+What are conditional statements in JavaScript?
+
+Difference between if, else if, and else.
+
+Write a program to check whether a number is even or odd using if-else.
+
+Explain the difference between if-else and switch.
+
+Write a program using switch to check the day of the week.*/
+
+
+/*
+🔹 Loops
+
+What are loops in JavaScript and why are they used?
+
+Difference between for, while, and do...while loops.
+
+Write a program to print numbers from 1 to 10 using a loop.
+
+Write a program to print the multiplication table of 5.
+
+Explain the difference between for...in and for...of.
+
+What is the difference between break and continue in loops? */
